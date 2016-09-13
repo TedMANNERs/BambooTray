a8c2541ab59c999e0446a44a6dea4cc5d211ae78
TedMANNERs <mannersofted@gmail.com>
2016-09-13 10:42:41 +0200

    Added current windows user as default value, set initial focus on password box

--- BambooTray.App/LoginViewModel.cs
+++ BambooTray.App/LoginViewModel.cs
@@ -1,9 +1,15 @@
-﻿using System.Security;
+﻿using System;
+using System.Security;
 
 namespace BambooTray.App
 {
     public class LoginViewModel : ILoginViewModel
     {
+        public LoginViewModel()
+        {
+            Username = Environment.UserName;
+        }
+
         public string Username { get; set; }
         public SecureString Password { get; set; } = new SecureString();
     }
